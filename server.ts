bb2a719c05c13a1731655ff33b1506e30d298892
Mia Adjei <adjei.m@gmail.com>
2021-05-07 15:12:46 -0400

    Small changes to server side initial code

--- server.ts
+++ server.ts
@@ -1,14 +1,19 @@
 import * as dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import twilio, { Twilio } from 'twilio';
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+const allowedOrigins = ['http://localhost:3000'];
 
 const app = express();
 app.use(express.json());
 
-app.use(express.json());
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
